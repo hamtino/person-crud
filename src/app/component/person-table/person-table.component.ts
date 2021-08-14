5d6a7961ac80b0c57de9f0fd3e6eec0d01b34a06
Juan Jaminton Yate <hamtino@gmail.com>
2021-08-13 19:48:02 -0500

    creamos el componente de la tabla

---
+++ src/app/component/person-table/person-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-person-table',
+  templateUrl: './person-table.component.html',
+  styleUrls: ['./person-table.component.css']
+})
+export class PersonTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
